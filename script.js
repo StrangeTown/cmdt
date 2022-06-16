ac8a0801e3cacf469749b36d4bf633f57a1caf05
xing <yinxingdyx@163.com>
2022-06-16 19:38:22 +0800

    Init w&s version.

--- script.js
+++ script.js
@@ -1,32 +1,52 @@
-const note = (function () {
-  const noteStorageKey = 'note'
-  const noteTextarea = $('#note_textarea')
-
-  const listenNoteChange = () => {
-    noteTextarea.on('input', function (e) {
-      const value = e.target.value
-      chrome.storage.sync.set({
-        [noteStorageKey]: value
-      })
-    })
-  }
-  const initNoteValue = () => {
-    chrome.storage.sync.get([noteStorageKey], function(result) {
-      const noteValue = _.get(result, 'note')
-      noteTextarea.val(noteValue)
-    })
-  }
-
+const sentence = (() => {
+  const sentences = [
+    {
+      sentence:
+        'The virus may also be transmitted by respiratory droplets expelled by an infected patient who comes into physical contact with another person',
+      word: 'respiratory',
+      definition: 'of or relating to breathing',
+      image: 'https://s1.ax1x.com/2022/06/15/XoxKwF.jpg',
+      imageAuthor: `<span class="rTNyH RZQOk">Photo by <a href="https://unsplash.com/@elijahdhiett?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Elijah Hiett</a> on <a href="https://unsplash.com/s/photos/breathing?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a>
+      </span>`,
+    },
+  ]
   const init = () => {
-    initNoteValue()
-    listenNoteChange()
+    const random = Math.floor(Math.random() * sentences.length)
+    const randomItem = sentences[random]
+    const { sentence, word, definition, image, imageAuthor } = randomItem
+    const sentenceWithWord = sentence.replace(
+      word,
+      `<span class="word">${word}</span>`
+    )
+    const sentenceClsElement = document.querySelector('.sentence')
+    sentenceClsElement.innerHTML = sentenceWithWord
   }
-
-  return { init }
+  return {
+    init,
+  }
 })()
-
+const listener = (() => {
+  const init = () => {
+    const mainEle = document.querySelector('.main')
+    const wordClsElement = document.querySelector('.word')
+    if (!wordClsElement || !mainEle) {
+      return
+    }
+    //hover event
+    wordClsElement.addEventListener('mouseover', (e) => {
+      mainEle.classList.add('focus')
+    })
+    wordClsElement.addEventListener('mouseout', (e) => {
+      mainEle.classList.remove('focus')
+    })
+  }
+  return {
+    init,
+  }
+})()
 const main = async () => {
-  note.init()
+  sentence.init()
+  listener.init()
 }
 
 $(function () {
